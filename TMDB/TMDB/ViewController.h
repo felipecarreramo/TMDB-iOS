a72ff12d7a6e7e2d49ca8bf71b14610e0175a386
Juan Felipe carrera <pipecamo@gmail.com>
2015-04-10 11:09:18 -0500

    initial project

---
+++ TMDB/TMDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TMDB
+//
+//  Created by Mobile Developer on 10/04/15.
+//  Copyright (c) 2015 Juan Carrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TMDB/TMDB/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TMDB
+//
+//  Created by Mobile Developer on 10/04/15.
+//  Copyright (c) 2015 Juan Carrera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
